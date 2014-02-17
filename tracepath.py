f50413f1d795d30b619755bb41c6ddcc29f28b69
anders hoff <shall.be.lifted@gmail.com>
2014-02-17 23:42:08 +0100

    working cirle approximation.

--- tracepath.py
+++ tracepath.py
@@ -3,9 +3,10 @@
 
 from numpy import cos, sin, pi, arctan2, sqrt,\
                   square, linspace, zeros, array,\
-                  concatenate, delete
+                  concatenate, delete, row_stack, column_stack
 
 from numpy.random import random, normal, randint, shuffle
+from scipy import interpolate
 import numpy as np
 import cairo
 from time import time as time
@@ -13,12 +14,13 @@ from operator import itemgetter
 from scipy.spatial import cKDTree
 import gtk, gobject
 
-#np.random.seed(1)
+np.random.seed(2)
 
 PI = pi
 TWOPI = pi*2.
+PIHALF = pi*0.5
 
-SIZE = 1000
+SIZE = 5000
 ONE = 1./SIZE
 
 BACK = 1.
@@ -33,21 +35,22 @@ NUM_NEAR_INDICES = 30
 SHIFT_INDICES = 5
 
 W = 0.9
-PIX_BETWEEN = 11
+PIX_BETWEEN = 2
 
-#START_X = (1.-W)*0.5
-#START_Y = (1.-W)*0.5
-START_X = 0.5
+START_X = (1.-W)*0.5
 START_Y = (1.-W)*0.5
+#START_X = 0.5
+#START_Y = 0.5
 
 NUMMAX = int(2*SIZE)
 NUM_LINES = int(SIZE*W/PIX_BETWEEN)
+#NUM_LINES = 150
 H = W/NUM_LINES
 
 FILENAME = 'aa'
 
 TURTLE_ANGLE_NOISE = pi*0.05
-INIT_TURTLE_ANGLE_NOISE = 0.05
+INIT_TURTLE_ANGLE_NOISE = 0.0
 
 def myrandom(size):
 
@@ -77,37 +80,12 @@ def turtle(sthe,sx,sy,steps):
     THE[k] = the
     the += noise[k]
 
-    if x>X_MAX or x<X_MIN or y>Y_MAX or y<Y_MIN:
-      XY = XY[:k,:]
-      THE = THE[:k]
-      break
+    #if x>X_MAX or x<X_MIN or y>Y_MAX or y<Y_MIN:
+      #XY = XY[:k,:]
+      #THE = THE[:k]
+      #break
 
   return THE, XY
-
-#def get_near_indices(tree,xy,d,k):
-
-  #dist,data_inds = tree.query(xy,k=k,distance_upper_bound=d,eps=ONE)
-
-  #return dist, data_inds.flatten()
-
-
-#def alignment(the,dist):
-
-  #dx = cos(the)
-  #dy = sin(the)
-
-  #### inverse proporional distance scale
-  ##dx = np.sum(dx/dist)
-  ##dy = np.sum(dy/dist)
-
-  ### linear distance scale
-  #dx = np.sum(dx*(1.-dist))
-  #dy = np.sum(dy*(1.-dist))
-
-  #dd = (dx*dx+dy*dy)**0.5
-
-  #return dx/dd,dy/dd
-
 
 class Render(object):
 
@@ -130,8 +108,6 @@ class Render(object):
 
   def line(self,xy):
 
-    self.ctx.set_source_rgba(0,0,0,0.6)
-    self.ctx.set_line_width(ONE*3.)
 
     self.ctx.move_to(xy[0,0],xy[0,1])
     for (x,y) in xy[1:]:
@@ -162,33 +138,75 @@ class Path(object):
       ii = set(inds)
       isect = ii.intersection(near_last)
 
-      print k,inds,isect
-
       if len(isect)<1:
         near_last = inds
         circles.append((k,inds))
 
-    self.circles = circles
+    alphas = []
+    for k,inds in circles:
 
-    xy_circles = [self.xy[k,:] for k,_ in circles]
+      inds_s = array(sorted(inds),'int')
+      xy_diff_sum = self.xy[inds_s[-1],:]-self.xy[inds_s[0],:]
 
-    return xy_circles
+      #xy_diff = self.xy[inds_s[1:],:]-self.xy[inds_s[:-1],:]
+      #xy_diff_sum = np.sum(xy_diff,axis=0)
+
+      alpha = arctan2(xy_diff_sum[1],xy_diff_sum[0])
+      alphas.append(alpha)
+
+    #noise = myrandom(len(alphas))*0.5
+    alphas = array(alphas) + the
+
+    dx = cos(alphas)
+    dy = sin(alphas)
+
+    xy_circles = row_stack([self.xy[k,:] for k,_ in circles])
+
+    xy_new = zeros(xy_circles.shape,'float')
+    xy_new[:,:] = xy_circles[:,:]
+
+    xy_new[:,0] += dx*r
+    xy_new[:,1] += dy*r
+
+    return xy_circles, xy_new
 
 
 def main():
 
   render = Render(SIZE)
 
+  render.ctx.set_source_rgb(1,0,0)
+  render.ctx.set_line_width(ONE)
+
   the,xy = turtle(0.5*PI,START_X,START_Y,NUMMAX)
   render.line(xy)
 
-  path = Path(xy)
+  for i in xrange(NUM_LINES):
 
-  circles = path.trace(5*ONE,PI)
+    if not i%10:
+      print i, NUM_LINES
 
-  for xy in circles:
-    render.circle(xy,5*ONE)
+    path = Path(xy)
+    circles,xy = path.trace(PIX_BETWEEN*ONE,-PIHALF)
 
+    tck,u = interpolate.splprep([xy[:,0],xy[:,1]],s=0)
+    unew = np.linspace(0,1,NUMMAX)
+    out = interpolate.splev(unew,tck)
+
+    xy = column_stack(out)
+
+    render.ctx.set_source_rgb(0,0,0)
+    render.line(xy)
+
+    alpha_noise = myrandom(len(xy))*pi
+    xy_noise = column_stack([cos(alpha_noise),sin(alpha_noise)])*ONE*0.5
+    xy += xy_noise
+
+    if not i%100:
+
+      fn = '{:s}_{:05d}.png'.format(FILENAME,i)
+      print fn
+      render.sur.write_to_png(fn)
 
   render.sur.write_to_png('{:s}_final.png'.format(FILENAME))
 
